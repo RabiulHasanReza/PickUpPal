dde6939479e729f7351ee79ab65dcba26c4d31e2
SeYaM610 <seyam.solaiman@gmail.com>
2025-07-30 03:04:34 +0600

    promo and admin

--- Backend/db.js
+++ Backend/db.js
@@ -1,28 +1,28 @@
-// require('dotenv').config();
-// const Pool = require('pg').Pool;
-
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// module.exports = pool;
-
-
+require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user : "postgres",
-    password : "Kmss_01666",
-    host : "localhost",
-    port : 5432,
-    database : "pickuppal"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
+
+
+// const Pool = require('pg').Pool;
+
+// const pool = new Pool({
+//     user : "postgres",
+//     password : "Kmss_01666",
+//     host : "localhost",
+//     port : 5432,
+//     database : "pickuppal"
+// });
+
+// module.exports = pool;
